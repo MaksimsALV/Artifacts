f1f5599e9753b28e84d3a32d623a718be80fb25f
MaksimsALV <maxim.ahmetov@gmail.com>
2025-08-02 10:08:26 +0300

    getToken wip

--- src/main/java/com/artifacts/game/endpoints/token/Token.java
+++ src/main/java/com/artifacts/game/endpoints/token/Token.java
@@ -1,16 +1,27 @@
 package com.artifacts.game.endpoints.token;
 
+import com.artifacts.api.http.Send;
 import com.artifacts.game.config.BaseURL;
 
 import java.net.http.HttpResponse;
 
-/*
-
 public class Token {
     public static HttpResponse<String> getToken() {
-        var endpoint = BaseURL.getBaseUrl("api.baseUrl" + "/token");
+        var baseUrl = BaseURL.getBaseUrl("api.baseUrl");
+        var endpoint = baseUrl + "/token";
+        try {
+            HttpResponse<String> response = Send.get(endpoint);
+            if (response.statusCode() == 200) {
+                return response;
+            } else if (response.statusCode() == 455) {
+                System.err.println("Failed to generate token");
+            } else {
+                System.err.println("Unexpected status code: " + response.statusCode());
+            }
+        } catch (Exception getTokenError) {
+            System.err.println(getTokenError.getMessage());
+        }
+        return null;
     }
 }
-
- */
 
