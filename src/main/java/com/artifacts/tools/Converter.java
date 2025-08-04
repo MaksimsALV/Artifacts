1b9ea4271a7778f6ab480a62331a63a394210712
MaksimsALV <maxim.ahmetov@gmail.com>
2025-08-04 11:19:02 +0300

    adding cooldownExpirationTimeToNowTimeToMillisConverter

--- src/main/java/com/artifacts/tools/Converter.java
+++ src/main/java/com/artifacts/tools/Converter.java
@@ -1,7 +1,20 @@
 package com.artifacts.tools;
 
+import com.artifacts.game.endpoints.characters.GetCharacter;
+
+import java.time.Instant;
+
 public class Converter {
     public static long SecondsToMillisConverter(double seconds) {
         return (long) (seconds * 1000);
+    }
+
+    public static long cooldownExpirationTimeToNowTimeToMillisConverter() {
+        var cooldownExpirationDateTime = GetCharacter.CHARACTER.get(0).get("cooldown_expiration");
+            var convertedCooldownExpirationDateTimeToMillis = Instant.parse(cooldownExpirationDateTime).toEpochMilli();
+            var convertedCurrentDateTimeToMillis = Instant.now().toEpochMilli();
+
+            var cooldownTimeInMillis = convertedCooldownExpirationDateTimeToMillis - convertedCurrentDateTimeToMillis;
+            return cooldownTimeInMillis;
     }
 }
