9620ee6b4a0ac8d4d7b5000022104607c00a326c
MaksimsALV <maxim.ahmetov@gmail.com>
2025-08-09 18:38:03 +0300

    introducing ActionGathering

--- src/main/java/com/artifacts/game/endpoints/mycharacters/ActionGathering.java
+++ src/main/java/com/artifacts/game/endpoints/mycharacters/ActionGathering.java
@@ -3,29 +3,23 @@ package com.artifacts.game.endpoints.mycharacters;
 import com.artifacts.api.http.Send;
 import com.artifacts.game.config.BaseURL;
 import com.artifacts.game.endpoints.characters.GetCharacter;
-import com.artifacts.tools.Converter;
 import org.json.JSONObject;
 
 import java.net.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.regex.Pattern;
 
-import static com.artifacts.api.errorhandling.ErrorCodes.CODE_CHARACTER_IN_COOLDOWN;
 import static com.artifacts.api.errorhandling.ErrorCodes.*;
 import static com.artifacts.api.errorhandling.GlobalErrorHandler.globalErrorHandler;
 
+public class ActionGathering {
+    public static List<HashMap<String, String>> GATHERING = new ArrayList<>();
 
-//todo redo using global logic
-public class ActionFight {
-    //todo need to think if I even need lists for each endpoint like this. Because i work with data directly from response 200 body data
-    public static List<HashMap<String, String>> FIGHT = new ArrayList<>();
-
-    public static HttpResponse<String> actionFight() {
+    public static HttpResponse<String> actionGathering() {
         var name = GetCharacter.CHARACTER.get(0).get("name");
         var baseUrl = BaseURL.getBaseUrl("api.baseUrl");
-        var endpoint = baseUrl + "/my/" + name + "/action/fight";
+        var endpoint = baseUrl + "/my/" + name + "/action/gathering";
 
         try {
             HttpResponse<String> response = Send.post(endpoint, "", true);
@@ -37,26 +31,25 @@ public class ActionFight {
                 var responseDataObject = object.getJSONObject("data");
                     var responseCharacterDataObject = responseDataObject.getJSONObject("character");
                     var responseCooldownDataObject = responseDataObject.getJSONObject("cooldown");
-                //todo if i will not use lists of actions then maybe i can remove whole bottom logic of putting data into the lists. Because i work with data directly from response 200 body data
-                FIGHT.clear();
+                GATHERING.clear();
 
                 HashMap<String, String> characterData = new HashMap<>();
                 for (var key : responseCharacterDataObject.keySet()) {
                     var value = responseCharacterDataObject.get(key).toString();
                     characterData.put(key, value);
                 }
-                FIGHT.add(characterData);
+                GATHERING.add(characterData);
 
                 HashMap<String, String> cooldownData = new HashMap<>();
                 for (var key : responseCooldownDataObject.keySet()) {
                     var value = responseCooldownDataObject.get(key).toString();
                     cooldownData.put(key, value);
                 }
-                FIGHT.add(cooldownData);
+                GATHERING.add(cooldownData);
                 var cooldown = responseCooldownDataObject.getInt("remaining_seconds");
                 var reason = responseCooldownDataObject.getString("reason");
                 var millis = cooldown * 1000;
-                if (responseCooldownDataObject.getInt("remaining_seconds") > 0) { //todo (cooldown > 0), should do that everywhere
+                if (cooldown > 0) {
                     System.out.println(name + " is now on a cooldown for: " + cooldown + "s due to " + reason);
                     Thread.sleep(millis);
                     return response;
@@ -66,25 +59,9 @@ public class ActionFight {
                 }
             }
             return response;
-        } catch (Exception actionFightException) {
-            System.err.println(endpoint + " | Exception: " + actionFightException.getMessage());
+        } catch (Exception actionGatheringException) {
+            System.err.println(endpoint + " | Exception: " + actionGatheringException.getMessage());
         }
         return null;
     }
 }
-
-
-//removed functionality
-
-/*
-//inventory quantity counter
-var responseCharacterInventoryDataObject = responseCharacterDataObject.getJSONArray("inventory");
-var numberOfItemsInInventory = 0; //starting point
-for (var i = 0; i < responseCharacterInventoryDataObject.length(); i++) {
-    var slotObject = responseCharacterInventoryDataObject.getJSONObject(i);
-    numberOfItemsInInventory += slotObject.getInt("quantity");
-}
-HashMap<String, String> inventoryCountMap = new HashMap<>();
-inventoryCountMap.put("inventory_count", String.valueOf(numberOfItemsInInventory));
-FIGHT.add(inventoryCountMap);
-*/
