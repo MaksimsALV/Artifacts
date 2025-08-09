4b6b68bd2a8d1cfb72f0762456646fb139f25c63
MaksimsALV <maxim.ahmetov@gmail.com>
2025-08-09 17:37:18 +0300

    full inventory banking logic added within the loop

--- src/main/java/com/artifacts/MainForConsole.java
+++ src/main/java/com/artifacts/MainForConsole.java
@@ -12,12 +12,12 @@ public class MainForConsole {
     public static void main(String[] args) throws Exception {
         Login.login();
         System.out.println("\nLogin successful");
-        delay(5);
+        //delay(5);
 
         GetMyCharacters.getMyCharacters();
         GetCharacter.getCharacter();
         System.out.println("\ninitial Character data is received successfully");
-        delay(5);
+        //delay(5);
 
         L5.runLoop();
     }
--- src/main/java/com/artifacts/game/logic/leveling/L5.java
+++ src/main/java/com/artifacts/game/logic/leveling/L5.java
@@ -1,6 +1,7 @@
 package com.artifacts.game.logic.leveling;
 
 import com.artifacts.game.endpoints.characters.GetCharacter;
+import com.artifacts.game.endpoints.mycharacters.ActionDepositBankItem;
 import com.artifacts.game.endpoints.mycharacters.ActionFight;
 
 import java.util.HashMap;
@@ -15,7 +16,7 @@ import static com.artifacts.tools.Delay.delay;
 public class L5 {
 public static void runLoop() throws InterruptedException {
     System.out.println("\nStarting L5 initial check");
-    delay(5);
+    //delay(5);
         while (!statusOk()) {
             System.out.println("\nStatus is not okay, fixing....");
             getCharacter();
@@ -29,7 +30,7 @@ public static void runLoop() throws InterruptedException {
     System.out.println("\nAll issues are fixed. Starting infinite loop");
     delay(10);
         while (true) {
-            actionFight();
+            var fightResponse = actionFight(); //initially executes this method during loop start, then storing response in variable
             HashMap<String, String> characterData = ActionFight.FIGHT.get(0);
             var hp = Integer.parseInt(characterData.get("hp"));
             if (hp <= 60) {
@@ -37,9 +38,17 @@ public static void runLoop() throws InterruptedException {
             }
 
             //todo if inventory is full > move to bank > deposit > return to loop
-            //if (actionFight().statusCode() == CODE_CHARACTER_INVENTORY_FULL) {
-
-            //}
+            if (fightResponse != null && fightResponse.statusCode() == CODE_CHARACTER_INVENTORY_FULL) {
+                actionMove(4,1);
+                var depositResponse = ActionDepositBankItem.actionDepositBankItem();
+                if (depositResponse != null && depositResponse.statusCode() == CODE_SUCCESS) {
+                    runLoop();
+                    return; //quitting the current loop
+                } else {
+                    System.err.println("Fatal error occurred. Exiting..."); //total system killer if I cannot dump inventory and repeat on the loop
+                    System.exit(999);
+                }
+            }
         }
     }
 
