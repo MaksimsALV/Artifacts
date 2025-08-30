f305abde78343cf33d746eed3e9c6a3154aadc94
MaksimsALV <maxim.ahmetov@gmail.com>
2025-08-30 17:33:02 +0300

    introduce getAllItems

---
+++ src/main/java/com/artifacts/game/endpoints/items/GetAllItems.java
@@ -0,0 +1,34 @@
+package com.artifacts.game.endpoints.items;
+
+import com.artifacts.api.http.Send;
+import com.artifacts.game.config.BaseURL;
+import org.json.JSONObject;
+
+import java.net.http.HttpResponse;
+
+import static com.artifacts.api.errorhandling.ErrorCodes.CODE_SUCCESS;
+import static com.artifacts.api.errorhandling.GlobalErrorHandler.globalErrorHandler;
+
+public class GetAllItems {
+    public static JSONObject getAllItems(String craftMaterial) {
+        var baseUrl = BaseURL.getBaseUrl("api.baseUrl");
+        var endpoint = baseUrl + "/items" + "?craft_material=" + craftMaterial;
+
+        try {
+            HttpResponse<String> response = Send.get(endpoint, false);
+
+            if (response.statusCode() == CODE_SUCCESS) {
+                System.out.println(endpoint + " | " + CODE_SUCCESS);
+                var jsonObject = new JSONObject(response.body());
+                jsonObject.put("statusCode", response.statusCode());
+                return jsonObject;
+            }
+            globalErrorHandler(response, endpoint);
+            return new JSONObject().put("statusCode", response.statusCode());
+
+        } catch (Exception getCharacterException) {
+            System.err.println(endpoint + " | Exception: " + getCharacterException.getMessage());
+            return null;
+        }
+    }
+}
