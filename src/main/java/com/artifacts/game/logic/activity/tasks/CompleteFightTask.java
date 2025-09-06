f17995b3ebf52454035074f95352c393b86dbb9d
MaksimsALV <maxim.ahmetov@gmail.com>
2025-09-06 23:27:37 +0300

    introduce CompleteFightTask

---
+++ src/main/java/com/artifacts/game/logic/activity/tasks/CompleteFightTask.java
@@ -0,0 +1,27 @@
+package com.artifacts.game.logic.activity.tasks;
+
+import com.artifacts.game.endpoints.maps.GetAllMaps;
+
+import static com.artifacts.api.errorhandling.ErrorCodes.CODE_SUCCESS;
+import static com.artifacts.game.endpoints.mycharacters.ActionAcceptNewTask.actionAcceptNewTask;
+import static com.artifacts.game.endpoints.mycharacters.ActionMove.actionMove;
+import static com.artifacts.tools.GlobalCooldownManager.globalCooldownManager;
+
+public class CompleteFightTask {
+    public static void completeFightTask(String name) throws InterruptedException {
+        var coordinates = GetAllMaps.getAllMaps("monsters");
+        var x = coordinates.getJSONArray("data").getJSONObject(0).getInt("x");
+        var y = coordinates.getJSONArray("data").getJSONObject(0).getInt("y");
+
+        var response = actionMove(name, x, y);
+        var statusCode = response.getInt("statusCode");
+        if (statusCode == CODE_SUCCESS) {
+            globalCooldownManager(name, response);
+        }
+        response = actionAcceptNewTask(name);
+        statusCode = response.getInt("statusCode");
+        if (statusCode == CODE_SUCCESS) {
+            globalCooldownManager(name, response);
+        }
+    }
+}
