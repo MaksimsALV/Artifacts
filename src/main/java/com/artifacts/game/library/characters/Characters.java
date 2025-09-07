4e392ba67bb9a415083f88a109106d11733919f4
MaksimsALV <maxim.ahmetov@gmail.com>
2025-09-07 23:08:37 +0300

    Updated lore for characters

--- src/main/java/com/artifacts/game/library/characters/Characters.java
+++ src/main/java/com/artifacts/game/library/characters/Characters.java
@@ -2,10 +2,10 @@ package com.artifacts.game.library.characters;
 
 public final class Characters {
     private Characters() {}
-        private static final String WARRIOR = "MaxiTheGuy"; //The Hero, also a blacksmith that crafts his own weapons and gear
-        private static final String MINER = "Bjorn"; //Strong miner from Scandinavia. Is well known for his jewelry-crafting skills
+        private static final String WARRIOR = "MaxiTheGuy"; //The Hero, also a blacksmith that crafts his own weapons and gear, also Miner as needed
+        private static final String MINER = "Bjorn"; //Strong miner from Scandinavia. Is well known for his jewelry-crafting skills. Can assist in killing/woodcutting too due to strength
         private static final String ALCHEMIST = "Linzy"; //Well known gnome alchemist from city named Dalaran. Knows all herbs.
-        private static final String LUMBERJACK = "Axel"; //The master of Axe itself! Best lumberjack in the region
+        private static final String LUMBERJACK = "Axel"; //The master of Axe itself! Best lumberjack in the region. Can assist in killing/mining too due to strength
         private static final String CHEF = "Sushimiko"; //Master Chef of these lands, the well known sushi expert. Incredible talented fisher-woman that cooks great fish dishes.
 
         public static String getWarrior() {
