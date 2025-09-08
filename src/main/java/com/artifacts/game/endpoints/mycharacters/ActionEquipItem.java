46701cb82f2c457f7513fd8c194fab7c5632d64c
MaksimsALV <maxim.ahmetov@gmail.com>
2025-09-08 22:09:57 +0300

    Introducing ActionEquipItem

---
+++ src/main/java/com/artifacts/game/endpoints/mycharacters/ActionEquipItem.java
@@ -0,0 +1,41 @@
+package com.artifacts.game.endpoints.mycharacters;
+
+import com.artifacts.api.http.Send;
+import com.artifacts.game.config.BaseURL;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.net.http.HttpResponse;
+
+import static com.artifacts.api.errorhandling.ErrorCodes.CODE_SUCCESS;
+import static com.artifacts.api.errorhandling.GlobalErrorHandler.globalErrorHandler;
+
+public class ActionEquipItem {
+    public static JSONObject actionEquipItem(String name, String itemCode, String itemSlot, Integer quantity) {
+        var baseUrl = BaseURL.getBaseUrl("api.baseUrl");
+        var endpoint = baseUrl + "/my/" + name + "/action/equip";
+        var requestBody = new JSONArray()
+                .put(new JSONObject()
+                        .put("code", itemCode)
+                        .put("slot", itemSlot)
+                        .put("quantity", quantity))
+                .toString();
+
+        try {
+            HttpResponse<String> response = Send.post(endpoint, requestBody, true);
+
+            if (response.statusCode() == CODE_SUCCESS) {
+                System.out.println(endpoint + " | " + CODE_SUCCESS);
+                var jsonObject = new JSONObject(response.body());
+                jsonObject.put("statusCode", response.statusCode());
+                return jsonObject;
+            }
+            globalErrorHandler(response, endpoint);
+            return new JSONObject().put("statusCode", response.statusCode());
+
+        } catch (Exception actionEquipItemException) {
+            System.err.println(endpoint + " | Exception: " + actionEquipItemException.getMessage());
+            return null;
+        }
+    }
+}
