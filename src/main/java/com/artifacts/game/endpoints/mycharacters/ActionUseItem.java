b0cef3722c5dddcf9b11cabe90696fd3afa2948b
MaksimsALV <maxim.ahmetov@gmail.com>
2025-09-14 17:43:08 +0300

    Introducing ActionUseItem

---
+++ src/main/java/com/artifacts/game/endpoints/mycharacters/ActionUseItem.java
@@ -0,0 +1,38 @@
+package com.artifacts.game.endpoints.mycharacters;
+
+import com.artifacts.api.http.Send;
+import com.artifacts.game.config.BaseURL;
+import org.json.JSONObject;
+
+import java.net.http.HttpResponse;
+
+import static com.artifacts.api.errorhandling.ErrorCodes.CODE_SUCCESS;
+import static com.artifacts.api.errorhandling.GlobalErrorHandler.globalErrorHandler;
+
+public class ActionUseItem {
+    public static JSONObject actionUseItem(String name, String itemCode, String quantity) {
+        var baseUrl = BaseURL.getBaseUrl("api.baseUrl");
+        var endpoint = baseUrl + "/my/" + name + "/action/use";
+        var requestBody = new JSONObject()
+                .put("code", itemCode)
+                .put("quantity", quantity)
+                .toString();
+
+        try {
+            HttpResponse<String> response = Send.post(endpoint, requestBody, true);
+
+            if (response.statusCode() == CODE_SUCCESS) {
+                System.out.println(endpoint + " | " + CODE_SUCCESS);
+                var jsonObject = new JSONObject(response.body());
+                jsonObject.put("statusCode", response.statusCode());
+                return jsonObject;
+            }
+            globalErrorHandler(response, endpoint);
+            return new JSONObject().put("statusCode", response.statusCode());
+
+        } catch (Exception actionUseItemException) {
+            System.err.println(endpoint + " | Exception: " + actionUseItemException.getMessage());
+            return null;
+        }
+    }
+}
