23ec4f0076d78639bcc970efa3b9676fe1cb006f
MaksimsALV <maxim.ahmetov@gmail.com>
2025-09-24 16:05:00 +0300

    updating Delay.java to delay2.0

--- src/main/java/com/artifacts/tools/Delay.java
+++ src/main/java/com/artifacts/tools/Delay.java
@@ -1,10 +1,16 @@
 package com.artifacts.tools;
 
 public class Delay {
+//    public static void delay(int seconds) throws InterruptedException {
+//        for (int i = seconds; i > 0; i--) {
+//            System.out.println("Delay: next step in " + i + "s");
+//            Thread.sleep(1000);
+//        }
+//    }
+
+    //delay 2.0
     public static void delay(int seconds) throws InterruptedException {
-        for (int i = seconds; i > 0; i--) {
-            System.out.println("Delay: next step in " + i + "s");
-            Thread.sleep(1000);
-        }
+        System.out.println("Delay for: " + seconds + " seconds");
+        Thread.sleep(seconds * 1000L);
     }
 }
