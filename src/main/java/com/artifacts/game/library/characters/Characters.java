d1b688afbe869e66ee7a6fb6abf04250e1d131b5
MaksimsALV <maxim.ahmetov@gmail.com>
2025-10-04 21:53:43 +0300

    Warrior Character name change

--- src/main/java/com/artifacts/game/library/characters/Characters.java
+++ src/main/java/com/artifacts/game/library/characters/Characters.java
@@ -2,7 +2,7 @@ package com.artifacts.game.library.characters;
 
 public final class Characters {
     private Characters() {}
-        private static final String WARRIOR = "MaxiTheGuy"; //The Hero, also a blacksmith that crafts his own weapons and gear, also Miner as needed
+        private static final String WARRIOR = "Max"; //The Hero, also a blacksmith that crafts his own weapons and gear, also Miner as needed
         private static final String MINER = "Bjorn"; //Strong miner from Scandinavia. Is well known for his jewelry-crafting skills. Can assist in killing/woodcutting too due to strength
         private static final String ALCHEMIST = "Linzy"; //Well known gnome alchemist from city named Dalaran. Knows all herbs.
         private static final String LUMBERJACK = "Axel"; //The master of Axe itself! Best lumberjack in the region. Can assist in killing/mining too due to strength
