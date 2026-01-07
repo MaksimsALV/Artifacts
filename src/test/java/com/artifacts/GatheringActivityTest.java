0d2d602af77f00ec737542fee3aa9fb3f6c2537f
MaksimsALV <maxim.ahmetov@gmail.com>
2026-01-07 22:33:02 +0200

    GatheringActivityTest

---
+++ src/test/java/com/artifacts/GatheringActivityTest.java
@@ -0,0 +1,46 @@
+package com.artifacts;
+
+import com.artifacts.game.endpoints.maps.GetAllMaps;
+import com.artifacts.game.endpoints.mycharacters.ActionGathering;
+import com.artifacts.game.endpoints.mycharacters.ActionMove;
+import com.artifacts.game.logic.activity.gathering.Gathering;
+import com.artifacts.service.GlobalCooldownManager;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+
+import static com.artifacts.api.errorhandling.ErrorCodes.CODE_CHARACTER_LOCKED;
+import static com.artifacts.api.errorhandling.ErrorCodes.CODE_SUCCESS;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+class GatheringActivityTest {
+
+    @Test
+    void Gathering486APIResponseErrorTest() throws Exception {
+        var coordinatesMock = new JSONObject().put("data", new JSONArray().put(new JSONObject().put("x", 1).put("y", 2)));
+        var moveMock = new JSONObject().put("statusCode", CODE_SUCCESS);
+        var code_character_lockedMock = new JSONObject().put("statusCode", CODE_CHARACTER_LOCKED);
+        var stopMock = new JSONObject().put("statusCode", 999);
+
+        try (var maps = mockStatic(GetAllMaps.class);
+             var move = mockStatic(ActionMove.class);
+             var gathering = mockStatic(ActionGathering.class);
+             var cooldown = mockStatic(GlobalCooldownManager.class)) {
+
+
+            //stub
+            maps.when(() -> GetAllMaps.getAllMaps(anyString())).thenReturn(coordinatesMock);
+            move.when(() -> ActionMove.actionMove(anyString(), anyInt(), anyInt())).thenReturn(moveMock);
+            gathering.when(() -> ActionGathering.actionGathering(anyString())).thenReturn(code_character_lockedMock, stopMock);
+
+            //simulation
+            Gathering.gather("anyName", "anyActivityLocation");
+
+            //verify
+            gathering.verify(() -> ActionGathering.actionGathering("anyName"), times(2));
+            cooldown.verify(() -> GlobalCooldownManager.globalCooldownManager(eq("anyName"), any()), atLeastOnce());
+        }
+    }
+}
+
