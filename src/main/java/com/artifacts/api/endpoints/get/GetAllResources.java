116641a4cbf263842d0703c81fef10c6cee2c263
MaksimsALV <maxim.ahmetov@gmail.com>
2026-02-06 17:31:07 +0200

    GetAllResources 3.0 and new getAllMiningResourcesAsList helper

--- src/main/java/com/artifacts/api/endpoints/get/GetAllResources.java
+++ src/main/java/com/artifacts/api/endpoints/get/GetAllResources.java
@@ -12,11 +12,13 @@ import static com.artifacts.api.http.Client.getRequest;
 import static com.artifacts.api.http.Client.send;
 import static com.artifacts.tools.Retry.retry;
 
-//GetAllResources 2.0
+//GetAllResources 3.0
 public class GetAllResources {
-    public static JSONObject getAllResources() {
+    public static JSONObject getAllResources(String skill) {
         var retryCount = 0;
-        var endpoint = "/resources";
+        var size = 100;
+        var skillParameter = (skill == null || skill.isBlank()) ? "" : "&skill=" + skill;
+        var endpoint = "/resources?size=" + size + skillParameter;
         var request = getRequest(endpoint, null);
 
         while (true) {
@@ -40,6 +42,35 @@ public class GetAllResources {
             }
         }
     }
+
+/*    //GetAllResources 2.0
+    public class GetAllResources {
+        public static JSONObject getAllResources() {
+            var retryCount = 0;
+            var endpoint = "/resources";
+            var request = getRequest(endpoint, null);
+
+            while (true) {
+                try {
+                    HttpResponse<String> response = send(request);
+
+                    if (response.statusCode() == CODE_SUCCESS) {
+                        System.out.println(endpoint + " | " + CODE_SUCCESS);
+                        var responseBody = new JSONObject(response.body());
+                        responseBody.put("statusCode", response.statusCode());
+                        return responseBody;
+                    }
+                    globalErrorHandler(response, endpoint);
+                    return new JSONObject().put("statusCode", response.statusCode());
+
+                } catch (Exception e) {
+                    System.err.println(endpoint + " | Exception: " + e);
+                    if (!retry(++retryCount)) {
+                        return null;
+                    }
+                }
+            }
+        }*/
 
 /*    //GetAllResources 1.0
     public class GetAllResources {
@@ -66,11 +97,22 @@ public class GetAllResources {
         }*/
 
     public static List<String> getAllResourcesAsList() {
-        var response = getAllResources().getJSONArray("data");
+        var response = getAllResources(null).getJSONArray("data");
         List<String> listOfResourceCodes = new ArrayList<>();
         for (int i = 0; i < response.length(); i++) {
             listOfResourceCodes.add(response.getJSONObject(i).getString("code"));
         }
+        return listOfResourceCodes;
+    }
+
+    public static List<String> getAllMiningResourcesAsList() {
+        var response = getAllResources("mining").getJSONArray("data");
+        List<String> listOfResourceCodes = new ArrayList<>();
+        response.forEach(object -> {
+            var item = (JSONObject) object;
+            var code = item.getJSONArray("drops").getJSONObject(0).getString("code");
+            listOfResourceCodes.add(code);
+        });
         return listOfResourceCodes;
     }
 }
