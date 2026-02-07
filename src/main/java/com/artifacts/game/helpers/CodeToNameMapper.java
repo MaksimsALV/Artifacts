453ecb5a28ec67455d34ffeb4e9f8d2b19024b2f
MaksimsALV <maxim.ahmetov@gmail.com>
2026-02-07 14:46:16 +0200

    CodeToNameMapper helper

---
+++ src/main/java/com/artifacts/game/helpers/CodeToNameMapper.java
@@ -0,0 +1,12 @@
+package com.artifacts.game.helpers;
+
+public class CodeToNameMapper {
+    public String codeToNameMap(String code) {
+        //add more mappers if needed for other needs.
+        if ("coal".equals(code)) return "coal_rocks";
+        if (code != null && code.endsWith("_ore")) {
+            return code.replace("_ore", "_rocks");
+        }
+        return code;
+    }
+}
