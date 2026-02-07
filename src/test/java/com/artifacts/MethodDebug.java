738f7d97820614e572f7e6115fb37cfa493eb59e
MaksimsALV <maxim.ahmetov@gmail.com>
2026-02-07 14:48:37 +0200

    MethodDebug and .gitignore update

--- src/test/java/com/artifacts/MethodDebug.java
+++ src/test/java/com/artifacts/MethodDebug.java
@@ -6,7 +6,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import com.artifacts.api.endpoints.get.GetMyCharacters;
 
+import static com.artifacts.api.endpoints.get.GetAllResources.*;
 import static com.artifacts.api.endpoints.post.Token.getToken;
+import static com.artifacts.game.logic.activity.Gathering.gather;
 //import static com.artifacts.game.library.items.GetItemsByItemType.getAllConsumableItems;
 //import static com.artifacts.game.library.items.GetItemsByItemType.getAllUtilityItems;
 //import static com.artifacts.game.logic.activity.crafting.CraftingGear.craftGear;
@@ -15,13 +17,17 @@ import static com.artifacts.api.endpoints.post.Token.getToken;
 @SpringBootTest(classes = Launcher.class, webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class MethodDebug {
     @Test
-    void debug() {
+    void debug() throws InterruptedException {
         getToken();
-        GetBankItems getBankItems = new GetBankItems();
-        System.out.println(getBankItems.countMiningResourcesFromBankAsHashMap());
-        System.out.println(getBankItems.countWoodcuttingResourcesFromBankAsHashMap());
-        System.out.println(getBankItems.countFishingResourcesFromBankAsHashMap());
-        System.out.println(getBankItems.countHerbsFromBankAsHashMap());
+//        System.out.println(getAllResourcesAsList("mining"));
+
+//        GetBankItems getBankItems = new GetBankItems();
+//        System.out.println(getBankItems.countMiningResourcesFromBankAsJson());
+//        System.out.println(getBankItems.countMiningResourcesFromBankAsHashMap());
+        gather("Max", null, true);
+//        System.out.println(getBankItems.countWoodcuttingResourcesFromBankAsHashMap());
+//        System.out.println(getBankItems.countFishingResourcesFromBankAsHashMap());
+//        System.out.println(getBankItems.countHerbsFromBankAsHashMap());
 //        GetMyCharacters getMyCharacters = new GetMyCharacters();
 //        System.out.println(getMyCharacters.getMyCharactersAsList());
 //        System.out.println("warrior name: " + getMyCharacters.getWarrior());
