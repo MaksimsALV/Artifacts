834a178125494b754cc8f8667c176f4eba07dfd9
MaksimsALV <maxim.ahmetov@gmail.com>
2026-04-25 23:41:30 +0300

    retry and sleep rewrite

--- src/main/java/com/artifacts/tools/Retry.java
+++ src/main/java/com/artifacts/tools/Retry.java
@@ -1,18 +1,16 @@
 package com.artifacts.tools;
 
-import static com.artifacts.tools.Delay.delay;
 
 public class Retry {
-    public static final int RETRY_ATTEMPTS = 7;
-    public static final int RETRY_DELAY = 10;
+    private final Sleep sleep;
 
-    public static boolean retry(int count) {
-        if (count >= RETRY_ATTEMPTS) {
-            return false;
-        } else {
-            delay(RETRY_DELAY);
-            return true;
-        }
+    public Retry(Sleep sleep) {
+        this.sleep = sleep;
+    }
+
+    public void retry(int count) {
+        final int delay = 30;
+        System.out.println("retry attempt: " + count);
+        sleep.sleep(delay);
     }
 }
-
--- src/main/java/com/artifacts/tools/Sleep.java
+++ src/main/java/com/artifacts/tools/Sleep.java
@@ -1,8 +1,8 @@
 package com.artifacts.tools;
 
-public class Delay {
-    public static void delay(int seconds) {
-        System.out.println("Delay for: " + seconds + " seconds");
+public class Sleep {
+    public void sleep(int seconds) {
+        System.out.println("Sleep for: " + seconds + " seconds");
         try {
             Thread.sleep(seconds * 1000L);
         } catch (InterruptedException ie) {
