0479ec1682dd56041cfc793ed215b4eab7697476
MaksimsALV <maxim.ahmetov@gmail.com>
2026-04-25 18:39:44 +0300

    logging

---
+++ src/main/java/com/artifacts/api/logs/ApiResponseLogger.java
@@ -0,0 +1,35 @@
+package com.artifacts.api.logs;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApiResponseLogger {
+    private final ObjectMapper objectMapper;
+
+    public ApiResponseLogger(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    public void logResponseOnError(Logger logger, ResponseEntity<?> response) {
+        if (response.getStatusCode() == HttpStatus.OK) {
+            try {
+                logger.error("returned status code {}, Body: {}",
+                        response.getStatusCode(),
+                        prettyJson(response.getBody()));
+            } catch (JsonProcessingException e) {
+                logger.error(e.getMessage());
+            }
+        }
+    }
+
+    public String prettyJson(Object value) throws JsonProcessingException {
+        return objectMapper
+                .writerWithDefaultPrettyPrinter()
+                .writeValueAsString(value);
+    }
+}
--- src/main/java/com/artifacts/game/server/service/GetServerStatus.java
+++ src/main/java/com/artifacts/game/server/service/GetServerStatus.java
@@ -1,8 +1,11 @@
 package com.artifacts.game.server.service;
 
+import com.artifacts.api.logs.ApiResponseLogger;
 import org.openapitools.client.ApiClient;
 import org.openapitools.client.api.ServerDetailsApi;
 import org.openapitools.client.model.StatusResponseSchema;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,15 +14,22 @@ import org.springframework.stereotype.Service;
 @Service
 public class GetServerStatus {
     private final ApiClient apiClient;
+    private final Logger logger =  LoggerFactory.getLogger(GetServerStatus.class);
+    private final ApiResponseLogger apiResponseLogger;
 
     @Autowired
-    public GetServerStatus(ApiClient apiClient) {
+    public GetServerStatus(ApiClient apiClient, ApiResponseLogger apiResponseLogger) {
         this.apiClient = apiClient;
+        this.apiResponseLogger = apiResponseLogger;
     }
 
     public ResponseEntity<StatusResponseSchema> getServerStatus() {
         ServerDetailsApi serverDetailsApi = new ServerDetailsApi(apiClient);
-        return serverDetailsApi.getServerDetailsGetWithHttpInfo();
+        ResponseEntity<StatusResponseSchema> response = serverDetailsApi.getServerDetailsGetWithHttpInfo();
+
+        apiResponseLogger.logResponseOnError(logger, response);
+
+        return response;
     }
 
     public boolean serverIsUp() {
