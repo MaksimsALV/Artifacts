884a2f48467f9a201a99c3f52fa2b2a5018d286c
MaksimsALV <maxim.ahmetov@gmail.com>
2026-04-30 20:17:55 +0300

    todo

--- src/main/java/com/artifacts/api/service/account/GetAccountCharacters.java
+++ src/main/java/com/artifacts/api/service/account/GetAccountCharacters.java
@@ -13,7 +13,6 @@ import org.springframework.web.client.RestClientResponseException;
 
 import static com.artifacts.api.HttpCodes.*;
 
-//todo tests
 @Service
 public class GetAccountCharacters {
     private final ApiClient apiClient;
