85b080e2f3aa90e0d1cb2ff269deb6bbb151b49a
MaksimsALV <maxim.ahmetov@gmail.com>
2026-05-02 23:20:28 +0300

    GetAllMaps

---
+++ src/main/java/com/artifacts/api/service/maps/GetAllMaps.java
@@ -0,0 +1,47 @@
+package com.artifacts.api.service.maps;
+
+import com.artifacts.api.logs.ApiResponseLogger;
+import com.artifacts.tools.Retry;
+import org.openapitools.client.ApiClient;
+import org.openapitools.client.api.MapsApi;
+import org.openapitools.client.model.StaticDataPageMapSchema;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientResponseException;
+
+import static com.artifacts.api.HttpCodes.SUCCESS;
+
+//todo tests
+@Service
+public class GetAllMaps {
+    private final ApiClient apiClient;
+    private final Logger logger = LoggerFactory.getLogger(GetAllMaps.class);
+    private final ApiResponseLogger apiResponseLogger;
+    private final Retry retry;
+
+    public GetAllMaps(ApiClient apiClient, ApiResponseLogger apiResponseLogger, Retry retry) {
+        this.apiClient = apiClient;
+        this.apiResponseLogger = apiResponseLogger;
+        this.retry = retry;
+    }
+
+    public ResponseEntity<StaticDataPageMapSchema> retrieveAllMaps() { //todo need to check if <T> is correct one here, maybe should be StaticDataPageMapSchema or smth
+        MapsApi mapsApi = new MapsApi(apiClient);
+
+        while (true) {
+            try {
+                ResponseEntity<StaticDataPageMapSchema> response = mapsApi.getAllMapsMapsGetWithHttpInfo(null, null, null, null, null, 10000);
+
+                if (response.getStatusCode().value() == SUCCESS) {
+                    return response;
+                }
+
+            } catch (RestClientResponseException e) {
+                apiResponseLogger.logErrorResponse(logger, e);
+                retry.retry();
+            }
+        }
+    }
+}
